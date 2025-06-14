2e77d59355124d18c031ded0e96a2f913f91bb5b
Ryan mckenna <75239678+RDM3DC@users.noreply.github.com>
2025-06-14 15:34:31 -0700

    Add hyperbolic geometry helpers

--- adaptivecad/geom/__init__.py
+++ adaptivecad/geom/__init__.py
@@ -2,5 +2,12 @@
 
 from .bezier import BezierCurve
 from .bspline import BSplineCurve
+from .hyperbolic import full_turn_deg, rotate_cmd, pi_a_over_pi
 
-__all__ = ["BezierCurve", "BSplineCurve"]
+__all__ = [
+    "BezierCurve",
+    "BSplineCurve",
+    "full_turn_deg",
+    "rotate_cmd",
+    "pi_a_over_pi",
+]
